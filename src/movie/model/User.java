b8f551816b96f3a19c0001b791071df46322d0b6
soyeon <thduswja23@gmail.com>
2025-01-10 17:48:22 +0900

    fix: main추가, contoller 유저 부분 추가

--- src/movie/model/User.java
+++ src/movie/model/User.java
@@ -1,6 +1,7 @@
 package movie.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Setter
 @Getter
+@Builder
 public class User {
 	private long userId; //사용자 id
 	private String userName; //사용자 이름
